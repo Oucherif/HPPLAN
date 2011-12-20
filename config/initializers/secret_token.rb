a889eacde2f93243ad850671361dbb0140ae0d8c
Farid Oucherif <faridoucherif@gmail.com>
2011-12-20 17:06:40 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HPPLAN::Application.config.secret_token = 'c3e1b75118099e5b0b35ffb1dc0c2efced6916245a76c9e3d89c174ac5fc989fba0051c851edaac0ca4c9ad81432102ea9336b52d2ecb3724f6348844687fdc0'
